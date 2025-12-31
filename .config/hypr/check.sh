aae951fb18e062f389077fa0f84dfb548fc4c74c
Col B Davis <coltranebraxtondavis@tuta.io>
2025-12-31 12:07:38 +0100

    Removed unnecessary files

--- .config/hypr/check.sh
+++
@@ -1,9 +0,0 @@
-if [[ $(stat -fc %T /sys/fs/cgroup) != "cgroup2fs" ]]; then
-  echo "cgroup v2 non attivo – aggiungo kernel param"
-  sudo sed -i 's/^GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="systemd.unified_cgroup_hierarchy=1 /' /etc/default/grub
-  sudo grub-mkconfig -o /boot/grub/grub.cfg   # per GRUB
-  # per systemd‑boot:
-  # sudo bootctl update
-  echo "Riavvia il PC e ritorna qui"
-  exit 0
-fi
